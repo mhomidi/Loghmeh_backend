9cd972132dc957207eaece23f64a1a8c3eded1e2
Alireza <alireza.zarenejad99@gmail.com>
2020-04-30 14:57:03 +0430

    add jwt. need to test and fix.

---
+++ src/main/java/controller/authFilter/AuthFilter.java
@@ -0,0 +1,55 @@
+package controller.authFilter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.http.HttpStatus;
+import services.Authentication;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebFilter(filterName="authFilter", urlPatterns="/*")
+public class AuthFilter implements Filter {
+    public AuthFilter(){
+    }
+
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+            throws IOException, ServletException {
+        try {
+            String uri = ((HttpServletRequest)servletRequest).getRequestURI();
+            if(uri.equals("/login") || uri.equals("/signup")){
+                chain.doFilter(servletRequest, servletResponse);
+                return;
+            }
+            JWTVerifier verifier = JWT.require(Authentication.algorithm)
+                    .withIssuer("loghmeh.com")
+                    .build();
+            String header = ((HttpServletRequest)servletRequest).getHeader("Authorization");
+            if(header != null) {
+                header = header.substring(7); // - Bearer
+                DecodedJWT jwt;
+                jwt = verifier.verify(header);
+                String id =  jwt.getClaim("id").asString();
+                servletRequest.setAttribute("id", id);
+                chain.doFilter(servletRequest, servletResponse);
+            }
+            else
+                ((HttpServletResponse)servletResponse).setStatus(HttpStatus.UNAUTHORIZED.value());
+        } catch (JWTVerificationException exception){
+            ((HttpServletResponse)servletResponse).setStatus(HttpStatus.FORBIDDEN.value());
+        }
+
+    }
+    public void init(FilterConfig fConfig) throws ServletException {
+    }
+
+}
